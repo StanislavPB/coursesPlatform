5b319744b4240d64da2ea9c95f1b15f48f812cd5
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-06-18 20:13:13 +0200

    StudentServiceAdd

---
+++ src/main/java/service/StudentServiceAdd.java
@@ -0,0 +1,18 @@
+package service;
+
+import repository.interfaces.InStudentRepository;
+import service.util.UserInput;
+import service.util.Validation;
+
+public class StudentServiceAdd {
+    private final InStudentRepository repository;
+    private final Validation validation;
+    UserInput ui = new UserInput();
+
+    public StudentServiceAdd(InStudentRepository repository, Validation validation) {
+        this.repository = repository;
+        this.validation = validation;
+    }
+
+
+}
