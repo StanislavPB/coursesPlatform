63ccc9d42e726319138c0ab6bba985364707b504
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-06-19 11:00:23 +0200

    add branch

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class StudentStudentRepository implements InStudentRepository {
+public class StudentRepository implements InStudentRepository {
     private int id = 0;
 
     private List<Student> students;
